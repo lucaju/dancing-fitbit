8b598b5ebb5e5231e45c890004300993915ab38c
Luciano Frizzera <lucaju@me.com>
2019-05-13 15:20:01 -0400

    Webpacl Optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const webpack = require('webpack');
-
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackBar = require('webpackbar');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-	mode: 'production', // production
+	mode: 'development', //development || production
 	entry: './src/app.js',
 	output: {
 		filename: 'app.bundle.js',
@@ -17,26 +18,51 @@ module.exports = {
 	devServer: {
 		contentBase: './dist',
 	},
-	devtool: 'inline-source-map',
+	devtool: 'inline-source-map', //inline-source-map || 'source-map' || ''
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
 				use: [
-					'style-loader',
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							// you can specify a publicPath here
+							// by default it uses publicPath in webpackOptions.output
+							publicPath: '../',
+							hmr: process.env.NODE_ENV === 'production',
+						},
+					},
 					'css-loader',
 				],
 			},
 			{
-				test: /\.(png|svg|jpg|gif)$/,
+				test: /\.(gif|png|jpe?g|svg)$/i,
 				use: [
 					'file-loader',
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							bypassOnDebug: true, // webpack@1.x
+							disable: true, // webpack@2.x and newer
+						},
+					},
 				],
 			},
 			{
 				test: /\.html$/,
 				use: 'mustache-loader',
 			},
+			{
+				test: /\.hbs$/,
+				loader: 'handlebars-loader',
+				options: {
+					knownHelpersOnly: false,
+					inlineRequires: /\/assets\/(:?images|audio|video)\//ig,
+					helperDirs: [path.join(__dirname, '/lib/hbs-helpers')],
+					// partialDirs: [path.join(PATHS.TEMPLATES, 'partials')],
+				},
+			}
 		],
 	},
 	plugins: [
@@ -67,12 +93,42 @@ module.exports = {
 			title: 'Dancing with Fitbit',
 			favicon: 'src/images/favico.png'
 		}),
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+		}),
 		new CopyWebpackPlugin([
 			{ from: 'src/dataset/', to: 'dataset/' },
 			{ from: 'src/images/', to: 'images/' },
 			{ from: 'src/asset/', to: 'asset/' }
 		]),
+		new WebpackBar(),
 		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
 		// new BundleAnalyzerPlugin(),
 	],
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+			minSize: 30000,
+			maxSize: 0,
+			minChunks: 1,
+			maxAsyncRequests: 5,
+			maxInitialRequests: 3,
+			automaticNameDelimiter: '~',
+			name: true,
+			cacheGroups: {
+				vendors: {
+					test: /[\\/]node_modules[\\/]/,
+					priority: -10
+				},
+				default: {
+					minChunks: 2,
+					priority: -20,
+					reuseExistingChunk: true
+				}
+			}
+		}
+	}
 };
